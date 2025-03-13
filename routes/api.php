7e4b6c45c53b830e716f26cb9a45615036e15bf9
rohsyl <sylvain@rohs.ch>
2025-03-13 13:55:30 +0100

    retrocompatibility for search endpoint (search.php)

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/upload', \App\Http\Controllers\Api\V1\Maps\UploadController::class);
 Route::get('/search', \App\Http\Controllers\Api\V1\Maps\SearchController::class)->middleware('throttle:map_search');
+Route::get('/search.php', \App\Http\Controllers\Api\V1\Maps\SearchController::class)->middleware('throttle:map_search');
 Route::get('/{game}/{map}', DownloadController::class);
