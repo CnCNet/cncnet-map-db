085b9a0b3006832cbb709961f06825a37b9b6fd8
rohsyl <sylvain@rohs.ch>
2025-03-13 14:23:35 +0100

    force https for prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Extensions\Repositories\Maps\MapStorageRepository;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('map_search', function (Request $request) {
             return Limit::perMinute(10)->by($request->ip());
         });
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
